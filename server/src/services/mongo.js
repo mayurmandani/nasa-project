cae26bcb3e5fd293714ad3bf6199ed5ed18a521e
Mayur Mandani <mmandani9@gmail.com>
2024-09-22 19:07:19 +0530

    Implemented the dotenv for managing secrets changes are updated

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
+require('dotenv').config();
+
 // Update below to match your own MongoDB connection string.
-const MONGO_URL = 'mongodb+srv://mmandani9:e16MHLXD1ok57dJ4@nasacluster.njvkp.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster'
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
     console.log('MongoDb connection ready!');
